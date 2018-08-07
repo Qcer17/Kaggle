fbbf908e64d967494bc19d154e98a6de3a4f8b34
Qcer17 <qin_tianyu@hotmail.com>
2018-08-07 18:25:45 +0800

    Update Stacking.py

--- tools/Stacking.py
+++ tools/Stacking.py
@@ -1,4 +1,7 @@
 from sklearn.model_selection import KFold
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
 import numpy as np
 
 class Stacking:
@@ -11,6 +14,12 @@ class Stacking:
 
     top_learner : object
         The top learner from which the stacking ensemble is built.
+
+    n_splits: int, default=5
+        Number of folds. Must be at least 2.
+
+    include_old_features : bool, default=False
+        If consider predictions of base learners as an expansion of old features.
 
     Attributes
     ----------
@@ -28,25 +37,41 @@ class Stacking:
 
     train_y_ : np.matrix
         The labels of training data.
+
+    n_splits_: int
+        Number of folds. Must be at least 2.
+
+    include_old_features_ : bool
+        If consider predictions of base learners as an expansion of old features.
     """
-    def __init__(self,base_learners,top_learner):
+    def __init__(self,base_learners,top_learner,n_splits=5,include_old_features=False,post_normalization=False):
         self.base_learners=base_learners
         self.top_learner=top_learner
+        self.n_splits=n_splits
+        self.include_old_features=include_old_features
+        self.post_normalization=post_normalization
 
-    def fit(self,X,y,n_splits=5):
+    def fit(self,X,y):
         """Fit estimator.
+            If include_old_features_==True then combine new features and
+            old features with min-max normalization, then fit.
 
         :param X: np.matrix of shape = [n_samples, n_features]
             Training data.
         :param y: array-like of shape = [n_samples]
             The target values.
-        :param n_splits: int, default=5
-            Number of folds. Must be at least 2.
         :return: None
         """
         self.meta_X=X
-        self.train_X=self.__get_train_X(X,y,n_splits=n_splits)
+        self.train_X=self.__get_train_X(X,y)
         self.train_y=np.array(y)
+        if self.include_old_features:
+            self.train_X=np.hstack((self.meta_X,self.train_X))
+            self.scaler=MinMaxScaler()
+            self.train_X=self.scaler.fit_transform(self.train_X)
+        if self.post_normalization:
+            self.standard_scaler=StandardScaler()
+            self.train_X=self.standard_scaler.fit_transform(self.train_X)
         self.top_learner.fit(self.train_X,self.train_y)
 
     def predict(self,X):
@@ -58,29 +83,42 @@ class Stacking:
             y : array of shape = [n_samples]
             The predictions.
         """
-        test_X=self.__get_train_X(X,None,n_splits=0)
+        test_X=self.__get_train_X(X,pd.DataFrame())
+        if self.include_old_features:
+            test_X=np.hstack((X,test_X))
+            test_X=self.scaler.transform(test_X)
+        if self.post_normalization:
+            test_X=self.standard_scaler.transform(test_X)
         return self.top_learner.predict(test_X)
 
-    def __get_train_X(self,X,y,n_splits):
-        X = np.array(X)
-        y = np.array(y)
+    def get_new_features(self,X):
+        """Get predictions of trained base learners.
+            Notice : call fit() first!
+
+        :param X: np.matrix of shape = [n_samples, n_features]
+            Meta features without labels.
+        :return: np.matrix of shape = [n_samples, n_base_learners]
+            New features.
+        """
+        ret_X=np.zeros((X.shape[0],len(self.base_learners)))
+        for i,learner in enumerate(self.base_learners):
+            ret_X[:,i]=learner.predict(X)
+        return ret_X
+
+    def __get_train_X(self,X,y):
         train_X = np.zeros((X.shape[0], len(self.base_learners)))
-        if n_splits>0:
-            kf = KFold(n_splits=n_splits, shuffle=True)
-            learner_cnt = 0
-            for learner in self.base_learners:
+        if len(y)>0:
+            X = np.array(X)
+            y = np.array(y)
+            kf = KFold(n_splits=self.n_splits, shuffle=True)
+            for learner_cnt,learner in enumerate(self.base_learners):
                 for train_index, test_index in kf.split(X):
                     learner.fit(X[train_index], y[train_index])
                     test_y = learner.predict(X[test_index])
-                    id_cnt = 0
-                    for id in test_index:
+                    for id_cnt,id in enumerate(test_index):
                         train_X[id][learner_cnt] = test_y[id_cnt]
-                        id_cnt = id_cnt + 1
-                learner_cnt = learner_cnt + 1
         else:
-            learner_cnt=0
-            for learner in self.base_learners:
+            for learner_cnt,learner in enumerate(self.base_learners):
                 learner.fit(self.meta_X,self.train_y)
                 train_X[:,learner_cnt]=learner.predict(X)
-                learner_cnt=learner_cnt+1
         return train_X
